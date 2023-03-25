1703480306c0e2913c815b26181931b89642d5ac
Stefan <langestefan@msn.com>
2023-03-25 03:34:58 +0100

    Re-organize constants.py

--- constants.py
+++ constants.py
@@ -1,27 +1,38 @@
 import logging
 import os
 import time
-import numpy as np
-from numpy.random import RandomState
-import random
-
 
 # v2g = True:  EV can be charged and discharged
 v2g = False
-v2h = True
+v2h = False
 
 # if we use the home battery or not
-USE_FLEX_HEATING = True
-USE_HOME_BATTERY = True
-USE_FLEX_BATT_CHARGING = True
+USE_FLEX_HEATING = False
+USE_HOME_BATTERY = False
+USE_FLEX_BATT_CHARGING = False
 
+# if we use real time consumption data or not
+USE_REAL_CONS = False
 
-# off-peak ptu's of the day
-# 0 = 00:00-00:15, ..., 95 = 23:45-00:00
-wd_start = 5
-wd_end = 15
-off_peak_wd = [4 * wd_start, int(4 * wd_end)]  # @ workday
-off_peak_we = [4 * 0, 4 * 24]  # @ weekend
+# for plotting consumption data of a single house
+PLOT_HOUSE = 1  # random.randint(1, 100)  # house number, starting at 1
+PLOT_DAY = 355  # random.randint(0, 364)  # day of the year, starting at 0
+PLOT_LEN = 3  # plot duration in days
+
+# range safety constant (% of battery size)
+R_SAFETY = 1.0
+
+# how often we refresh the min/max scalar for the next 24 hours (in ptu's)
+# for example: 48 means we refresh every 12 hours, 24 means we refresh every 6 hours... etc
+PSCALER_PRICE_INT = 96
+
+# PTU's of overlap between two consecutive price interval windows
+PSCALER_PRICE_OVERLAP = 96
+PSCALER_CONS_INT = 8
+CONS_WINDOW = 48
+
+# number of houses to simulate
+N_HOUSES = 100
 
 # LOGGING settings
 LOG_LEVEL_GLOBAL = logging.INFO
@@ -45,36 +56,15 @@ log_location = os.path.join(sim_path, "sim" + ".log")
 handler = logging.FileHandler(log_location)
 handler.setLevel(logging.DEBUG)
 
+# write entire constants.py to .txt file
+with open("constants.py", "r") as f:
+    constants = f.read()
+with open(os.path.join(sim_path, "constants.txt"), "w") as f:
+    f.write(constants)
+
+
 # add formatter to handler
 formatter = logging.Formatter(
     "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s", datefmt="%H:%M:%S"
 )
 handler.setFormatter(formatter)
-
-# range safety constant (% of battery size)
-R_SAFETY = 0.7
-
-# how often we refresh the min/max scalar for the next 24 hours (in ptu's)
-# for example: 48 means we refresh every 12 hours, 24 means we refresh every 6 hours... etc
-PSCALER_PRICE_INT = 96
-
-# PTU's of overlap between two consecutive price interval windows
-PSCALER_PRICE_OVERLAP = 96
-PSCALER_CONS_INT = 8
-CONS_WINDOW = 48
-
-# if we use real time consumption data or not
-USE_REAL_CONS = False
-
-# for plotting consumption data of a single house
-PLOT_HOUSE = 1  # random.randint(1, 100)  # house number, starting at 1
-PLOT_DAY = 355  # random.randint(0, 364)  # day of the year, starting at 0
-PLOT_LEN = 3  # plot duration in days
-
-# for p_scalar randomness
-P_SCALAR_PETURBANCE = 0.2
-np.random.seed(42)
-pert = np.random.normal(0, P_SCALAR_PETURBANCE, 100)
-
-# number of houses to simulate
-N_HOUSES = 100
